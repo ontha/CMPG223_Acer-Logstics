b453c4a35389a297b4b31f41d9ff23bcb63f6bcf
Thabiso3028 <65237633+Thabiso3028@users.noreply.github.com>
2020-09-07 21:22:29 +0200

    User login page

--- Information Management System(Acer Logistics)/Form1.Designer.cs
+++ Information Management System(Acer Logistics)/Form1.Designer.cs
@@ -38,11 +38,12 @@
 			this.txtPassword = new System.Windows.Forms.TextBox();
 			this.label6 = new System.Windows.Forms.Label();
 			this.label7 = new System.Windows.Forms.Label();
+			this.label5 = new System.Windows.Forms.Label();
 			this.SuspendLayout();
 			// 
 			// button1
 			// 
-			this.button1.Location = new System.Drawing.Point(120, 181);
+			this.button1.Location = new System.Drawing.Point(119, 201);
 			this.button1.Name = "button1";
 			this.button1.Size = new System.Drawing.Size(75, 23);
 			this.button1.TabIndex = 0;
@@ -53,7 +54,7 @@
 			// 
 			this.label1.AutoSize = true;
 			this.label1.Font = new System.Drawing.Font("Modern No. 20", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-			this.label1.Location = new System.Drawing.Point(127, 26);
+			this.label1.Location = new System.Drawing.Point(132, 25);
 			this.label1.Name = "label1";
 			this.label1.Size = new System.Drawing.Size(52, 18);
 			this.label1.TabIndex = 1;
@@ -112,7 +113,7 @@
 			this.label6.AutoSize = true;
 			this.label6.Font = new System.Drawing.Font("Century", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
 			this.label6.ForeColor = System.Drawing.SystemColors.Highlight;
-			this.label6.Location = new System.Drawing.Point(48, 225);
+			this.label6.Location = new System.Drawing.Point(58, 172);
 			this.label6.Name = "label6";
 			this.label6.Size = new System.Drawing.Size(62, 15);
 			this.label6.TabIndex = 9;
@@ -123,17 +124,28 @@
 			this.label7.AutoSize = true;
 			this.label7.Font = new System.Drawing.Font("Century", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
 			this.label7.ForeColor = System.Drawing.SystemColors.Highlight;
-			this.label7.Location = new System.Drawing.Point(146, 225);
+			this.label7.Location = new System.Drawing.Point(156, 172);
 			this.label7.Name = "label7";
 			this.label7.Size = new System.Drawing.Size(97, 15);
 			this.label7.TabIndex = 10;
 			this.label7.Text = "Forgot password?";
 			// 
+			// label5
+			// 
+			this.label5.AutoSize = true;
+			this.label5.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+			this.label5.Location = new System.Drawing.Point(97, 242);
+			this.label5.Name = "label5";
+			this.label5.Size = new System.Drawing.Size(125, 13);
+			this.label5.TabIndex = 11;
+			this.label5.Text = "Please enter your details!";
+			// 
 			// Login
 			// 
 			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.ClientSize = new System.Drawing.Size(335, 284);
+			this.ClientSize = new System.Drawing.Size(335, 302);
+			this.Controls.Add(this.label5);
 			this.Controls.Add(this.label7);
 			this.Controls.Add(this.label6);
 			this.Controls.Add(this.txtPassword);
@@ -164,6 +176,7 @@
 		private System.Windows.Forms.TextBox txtPassword;
 		private System.Windows.Forms.Label label6;
 		private System.Windows.Forms.Label label7;
+		private System.Windows.Forms.Label label5;
 	}
 }
 
