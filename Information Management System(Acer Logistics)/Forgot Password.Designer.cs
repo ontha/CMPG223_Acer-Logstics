26d7b8e720173720e291b74eacb6fb96f09543bc
Thabiso3028 <65237633+Thabiso3028@users.noreply.github.com>
2020-09-09 01:24:29 +0200

    Updated

---
+++ Information Management System(Acer Logistics)/Forgot Password.Designer.cs
@@ -0,0 +1,46 @@
+﻿namespace Information_Management_System_Acer_Logistics_
+{
+	partial class Forgot_Password
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.SuspendLayout();
+			// 
+			// Forgot_Password
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(405, 291);
+			this.Name = "Forgot_Password";
+			this.Text = "Forgot_Password";
+			this.ResumeLayout(false);
+
+		}
+
+		#endregion
+	}
+}
